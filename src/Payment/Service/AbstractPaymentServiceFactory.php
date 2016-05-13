2abbec7998e2e631a0ed35bb07654c0d59972901
Nick Claywell <nclaywell@soliantconsulting.com>
2016-05-13 15:14:22 -0500

    fix return value for createService()

--- src/Payment/Service/AbstractPaymentServiceFactory.php
+++ src/Payment/Service/AbstractPaymentServiceFactory.php
@@ -9,6 +9,12 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 class AbstractPaymentServiceFactory implements AbstractFactoryInterface
 {
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return bool
+     */
     public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
     {
         $config = $serviceLocator->get('config');
@@ -22,12 +28,18 @@ class AbstractPaymentServiceFactory implements AbstractFactoryInterface
         return (class_exists($config['payment_base']['services'][$requestedName]));
     }
 
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return array|object
+     */
     public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
     {
         $config = $serviceLocator->get('config');
 
         $requestedClassName = $config['payment_base']['services'][$requestedName];
-        $requestedClass = new $requestedClassName();
+        $requestedClass = $serviceLocator->get($requestedClassName);
 
         if (!($requestedClass instanceof AbstractRequest)) {
             throw new RuntimeException(sprintf(
