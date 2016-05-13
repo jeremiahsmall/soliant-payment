b79bcdce8ebd453ae60cbd97b340144eee6e7417
Nick Claywell <nclaywell@soliantconsulting.com>
2016-05-13 09:33:19 -0500

    skeleton module file structure

---
+++ config/module.config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'router' => [
+        'routes' => [],
+    ],
+    'service_manager' => [
+        'factories' => [],
+    ],
+    'controllers' => [
+        'invokables' => [],
+        'factories' => [],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+    'view_helpers' => [],
+];
---
+++ src/PaymentBase/Payment/Response/AbstractResponse.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PaymentBase\Payment\Response;
+
+class AbstractResponse
+{
+
+}
