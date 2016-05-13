0fdc289fc32e7ce7ff532e636a766621a441fa76
Steve Bennett <sbennett@soliantconsulting.com>
2016-05-13 10:11:18 -0500

    Initial commit

---
+++ config/module.config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'router' => [
+        'routes' => [],
+    ],
+    'service_manager' => [
+        'factories' => [],
+    ],
+    'controllers' => [
+        'invokables' => [],
+        'factories' => [],
+    ],
+    'view_helpers' => [],
+];
